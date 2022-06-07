5a5a0287e1e66f4b178e56cc36787a0028b6d52f
enrique miguel chalco vila <enrique.chalco@unmsm.edu.pe>
2022-06-06 21:38:15 -0500

    Agregando ejercicios de JS

---
+++ Ejercicios/ejercicios.js
@@ -0,0 +1,111 @@
+/*Ejercicio
+Dado un numero, devolver  la tabla de multiplicar completa
+Ejemplo:
+-Ejecuto la funcion:
+tablaMultiplicar(4)
+-Me devuelve este resultado:
+1 x 5 = 5
+2 x 5 = 10
+3 x 5 = 15
+4 x 5 = 20
+5 x 5 = 25
+6 x 5 = 30
+7 x 5 = 35
+8 x 5 = 40
+9 x 5 = 45
+*/
+
+/*Ejercicio
+Tienes una cadena de texto , comprobar si es un palindromo o no. Los palindromos
+son palabras que se leen igual aun estando invertidas.
+Ejemplo: ana , bob , otto, luzazul ,
+No tengas encuenta espacios o simbolos(+-?'*"")
+-Ejecutas la funcion:
+palindromo("miguel")//devuelve: false
+palindromo("ana")//devuelve: true
+*/
+
+/**Ejercicio
+*Dado una palabra buscarla en una frase y devolver cuantas veces aparece en ella.
+*La palabra y la frase deben ser parametros de la funcion 
+*Ejemplo: 
+*countCoincide("el multiverso de marvel es mejor que el multiverso de DC","multiverso")//devuelve: 2
+*countCoincide("juan hace el paso de anita","anito")//devuelve:0
+*/
+
+/*Ejercicio
+Dado una cadena de texto, darle la vuelta e invertir el orden de sus caracteres, sin usar metodos propios del lenguaje, 
+solo estructuras de control
+Ejemplo:
+invertir("hola") //devuelve: "aloh"
+invertir("quique") //devuelve: "euqiuq"
+ */
+
+/**Ejercicio
+ * ¿Cuanto es el X por ciento de Y?
+ * Ejemplo:
+ * equisPorciento(10,120) //devuelve: 12
+ * equisPorciento(20,100)//devuelve: 20
+ */
+
+/**Ejercicio
+ * Dado dos numeros devolver cuantos numeros IMPARES hay entre ellas.
+ * Ejemplo:
+ * impares(1,10) //devuelve:4
+ */
+
+/**Ejercicio
+ * Dado un numero entero, inviertelo y devuelve de nuevo el numero
+ * Ejemplo:
+ * invertirNumero(78)//devuelve: 87
+ */
+
+/**Ejercicio
+ * Dado dos arreglos, devolver un arreglo con los elementos comunes entre ambos
+ * Ejemplo:
+ * elementosComunes([1,2,3,4,5,7],[5.6.7.8]) //devuelve: [5,7]
+ */
+
+/**Ejercicio
+ * Dado un numero, devolver una escalera con escalones de "[-]", usando el numero como base y nivel de la escalera
+ * Ejemplo:
+ * escalera(4) //devuelve:
+ * [-]
+ * [-][-]
+ * [-][-][-]
+ * [-][-][-][-]
+ */
+
+/**Ejercicio
+ * Dado un numero mostrar todos los numeros desde ese numero hasta el 0 pero en intervalos de 9 en 9 en una lista con guiones
+ * donde cada numero debe empezar con n°
+ * Ejemplo:
+ * hastaCero(50) //devuelve
+ * --- Del 50 al 0 ---
+ * -n°50
+ * -n°41
+ * -n°32
+ * -n°23
+ * -n°14
+ * -n°5
+ * ---FIN---
+ */
+
+/**Ejercicio
+ * Dado un arreglo, dividirlo en tantos sub arreglos como sea necesario basandonos en un numero que indique su tamaño.
+ * Dividirlo en arreglos de X elementos
+ * Ejemplo:
+ * divideArreglo([4,5,6,7,9],2) //devuelve [[4,5],[6,7],[9]]
+ */
+
+/**Ejercicio
+ * Dado una cadena de texto, devolver el caracter mas usado.
+ * Ejemplo:
+ * masUsado("programandoandoconganas") //devuelve: Caracter que mas se repite es :a
+ */
+
+/**Ejercicio
+ * Dado una cadena de texto , devolver  cuantas vocales(a,e,i,o,u) tiene
+ * Ejemplo:
+ * vocales("programandoandoconganas") //devuelve: 9
+ */
