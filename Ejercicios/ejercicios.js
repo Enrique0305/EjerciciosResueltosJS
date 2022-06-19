86214d2074cce4c0b7954cc5b36d5e9c0d24f2de
enrique miguel chalco vila <enrique.chalco@unmsm.edu.pe>
2022-06-19 12:06:35 -0500

    Ejercicios

--- Ejercicios/ejercicios.js
+++ Ejercicios/ejercicios.js
@@ -1,8 +1,8 @@
-/*Ejercicio
+/*Ejercicio 1
 Dado un numero, devolver  la tabla de multiplicar completa
 Ejemplo:
 -Ejecuto la funcion:
-tablaMultiplicar(4)
+tablaMultiplicar(5)
 -Me devuelve este resultado:
 1 x 5 = 5
 2 x 5 = 10
@@ -13,9 +13,27 @@ tablaMultiplicar(4)
 7 x 5 = 35
 8 x 5 = 40
 9 x 5 = 45
+
+Como hacerlo:
+-Funcion con parametro "numero"
+-variable con texto encabezado
+-bucle de 1 al 10
+-concatenar a la variable string con la multiplicacion y el resultado
 */
 
-/*Ejercicio
+function ejercicio1TablaMultiplicar(numero){
+    let result = `Tabla del numero ${numero}: \n`;
+    for(let i=0; i<10;i++){
+        let multiplicacion= i*numero;
+        // console.log(multiplicacion);
+        result += `${i} X ${numero} = ${multiplicacion} \n`;
+    }
+
+    return result;
+}
+console.log(ejercicio1TablaMultiplicar(5))
+
+/*Ejercicio 2
 Tienes una cadena de texto , comprobar si es un palindromo o no. Los palindromos
 son palabras que se leen igual aun estando invertidas.
 Ejemplo: ana , bob , otto, luzazul ,
@@ -23,15 +41,61 @@ No tengas encuenta espacios o simbolos(+-?'*"")
 -Ejecutas la funcion:
 palindromo("miguel")//devuelve: false
 palindromo("ana")//devuelve: true
-*/
 
-/**Ejercicio
+Como hacerlo:
+-Funcion con parametro "texto"
+-Separar el texto en un arreglo de letras
+-Darle la vuelta
+-Unir el arreglo de letras  en un string y comparar con el parametro
+
+*/
+function ejercicio2Palindromo(cadena){
+    let invertido = cadena.split('').reverse().join('');
+    if(invertido===cadena){
+        return true;
+    }  else{
+        return false;
+    };
+}
+console.log(`Es un palindromo? ${ejercicio2Palindromo('juan')}`)
+
+/**Ejercicio 3
 *Dado una palabra buscarla en una frase y devolver cuantas veces aparece en ella.
 *La palabra y la frase deben ser parametros de la funcion 
 *Ejemplo: 
 *countCoincide("el multiverso de marvel es mejor que el multiverso de DC","multiverso")//devuelve: 2
 *countCoincide("juan hace el paso de anita","anito")//devuelve:0
+Como hacerlo:
+-Funcion con dos parametros 'frase' y 'busqueda'
+-Poner el string y limpiarlo
+-Comprobar si la busqueda esta incluida en la frase
+-Crear un array de la palabras de la frase
+-Mapear esas palabras y hacer un contador  de cada una
+-Devolver el contador de la busqueda
 */
+function ejercicio3CountCoincide(frase, busqueda){
+    let limpiandoTexto = frase.toLowerCase().replace(/[¡!.,-]/gi,'');
+    let resultado=0;
+    if(limpiandoTexto.includes(busqueda)){
+        let palabras=limpiandoTexto.split(' ')
+        let map = {};
+
+        for (const palabra of palabras) {
+            if(map[palabra]){
+                map[palabra]++;
+            }else{
+                map[palabra] = 1
+            }
+        }
+        // console.log(palabras);
+        // console.log(map);
+        resultado = map[busqueda];
+    } else{
+        resultado = 0;
+    }
+    return resultado;
+}
+console.log(ejercicio3CountCoincide("el multiverso de marvel es mejor que el multiverso de DC, multiverso multiverso","multiverso"));//4
 
 /*Ejercicio
 Dado una cadena de texto, darle la vuelta e invertir el orden de sus caracteres, sin usar metodos propios del lenguaje, 
@@ -39,6 +103,10 @@ solo estructuras de control
 Ejemplo:
 invertir("hola") //devuelve: "aloh"
 invertir("quique") //devuelve: "euqiuq"
+Como hacerlo:
+-Funcion con parametro texto
+-Crear una variable para guardar  la cadena invertida
+-Bucle que recora el string y c¿guarde los caracteres en la variable
  */
 
 /**Ejercicio
@@ -46,6 +114,10 @@ invertir("quique") //devuelve: "euqiuq"
  * Ejemplo:
  * equisPorciento(10,120) //devuelve: 12
  * equisPorciento(20,100)//devuelve: 20
+ * Como hacerlo:
+-Funcion con parametros porcentaje y numero
+-Crear una variable para  la operacion del procentaje 
+-Devolver el string que responda a la pregunta del enunciado
  */
 
 /**Ejercicio
